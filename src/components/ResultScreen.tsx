76d16ddcda807129d6b59980b82b325f3874ad15
fortuneAI <mozaiku.mozaiko@gmail.com>
2026-01-17 11:15:50 +0900

    Updated ResultScreen.tsx

--- src/components/ResultScreen.tsx
+++ src/components/ResultScreen.tsx
@@ -416,8 +416,8 @@ export function ResultScreen({ result, profile, onRestart, isFromHistory = false
                     alt="ENFP"
                     className="mx-auto"
                     style={{
-                      width: '384px',
-                      height: '384px',
+                      width: '500x',
+                      height: '500px',
                       filter: 'drop-shadow(0 0 20px rgba(191, 167, 110, 0.6))',
                     }}
                   />
